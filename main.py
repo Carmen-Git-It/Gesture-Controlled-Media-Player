c07f02f010ea6f6cf81b89ceabbb050184313501
Rahul Bajaj <rahulbajaj0242@gmail.com>
2023-08-07 20:20:31 -0400

    Update UI color scheme and fix 'fist' gesture bug

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import tkinter
 import tkinter.filedialog
 from multiprocessing import Process, Queue
 
+
+
 class MusicPlayer:
     def __init__(self, media_directory):
         self.media_directory = media_directory
@@ -20,33 +22,39 @@ class MusicPlayer:
 
         pygame.display.set_caption("Media Player")
 
-        self.background_color = (255, 255, 255)
+        self.background_color = (26, 32, 44)
         self.font = pygame.font.Font(None, 36)
 
-        # Load the icons
-        self.pause_icon = pygame.image.load("./images/icons/pause-button.png")
-        self.pause_icon = pygame.transform.scale(self.pause_icon, (50, 50))
+        # Load the icons with modified colors
+        self.pause_icon = pygame.image.load("./images/icons/pause-48.png")
+        self.pause_icon = pygame.transform.scale(self.pause_icon, (46, 46))
 
-        self.next_icon = pygame.image.load("./images/icons/right.png")
-        self.next_icon = pygame.transform.scale(self.next_icon, (50, 50))
 
-        self.prev_icon = pygame.image.load("./images/icons/left-arrow.png")
-        self.prev_icon = pygame.transform.scale(self.prev_icon, (50, 50))
+        self.next_icon = pygame.image.load("./images/icons/media-step-forward-48.png")
+        self.next_icon = pygame.transform.scale(self.next_icon, (46, 46))
 
-        self.play_icon = pygame.image.load("./images/icons/play.png")
-        self.play_icon = pygame.transform.scale(self.play_icon, (50, 50))
-        
-        self.volume_up_icon = pygame.image.load("./images/icons/volume-up.png")
-        self.volume_up_icon = pygame.transform.scale(self.volume_up_icon, (50, 50))
-        
-        self.volume_down_icon = pygame.image.load("./images/icons/volume-down.png")
-        self.volume_down_icon = pygame.transform.scale(self.volume_down_icon, (50, 50))
 
-        self.browse_icon = pygame.image.load("./images/icons/browse.png")
-        self.browse_icon = pygame.transform.scale(self.browse_icon, (50, 50))
+        self.prev_icon = pygame.image.load("./images/icons/media-step-backward-48.png")
+        self.prev_icon = pygame.transform.scale(self.prev_icon, (46, 46))
 
-        self.replay_icon = pygame.image.load("./images/icons/replay.png")
-        self.replay_icon = pygame.transform.scale(self.replay_icon, (45, 45))
+
+        self.play_icon = pygame.image.load("./images/icons/play-48.png")
+        self.play_icon = pygame.transform.scale(self.play_icon, (46, 46))
+
+        self.volume_up_icon = pygame.image.load("./images/icons/audio-add-48.png")
+        self.volume_up_icon = pygame.transform.scale(self.volume_up_icon, (52, 52))
+
+
+        self.volume_down_icon = pygame.image.load("./images/icons/audio-remove-48.png")
+        self.volume_down_icon = pygame.transform.scale(self.volume_down_icon, (52, 52))
+
+
+        self.browse_icon = pygame.image.load("./images/icons/folder-7-48.png")
+        self.browse_icon = pygame.transform.scale(self.browse_icon, (46, 46))
+
+
+        self.replay_icon = pygame.image.load("./images/icons/undo-2-48.png").convert_alpha()
+        self.replay_icon = pygame.transform.scale(self.replay_icon, (46, 46))
 
 
 
@@ -65,7 +73,7 @@ class MusicPlayer:
                 if pygame.mixer.music.get_busy():
                     self.pause()
             elif msg == 'fist':
-                self.quit()
+                self.replay_song()
             elif msg == 'like':
                 self.increase_volume()
             elif msg == 'dislike':
@@ -151,12 +159,12 @@ class MusicPlayer:
             self.window.fill(self.background_color)
 
             # Display the current media file name
-            text = self.font.render(self.media_files[self.current_media_index], True, (0, 0, 0))
+            text = self.font.render(self.media_files[self.current_media_index], True, (255, 255, 255))
             text_rect = text.get_rect(center=(self.window_width // 2, self.window_height // 2))
             self.window.blit(text, text_rect)
 
             # Display the volume level
-            volume_text = self.font.render(f"Volume: {int(pygame.mixer.music.get_volume() * 100)}%", True, (0, 0, 0))
+            volume_text = self.font.render(f"Volume: {int(pygame.mixer.music.get_volume() * 100)}%", True, (255, 255, 255))
             volume_text_rect = volume_text.get_rect(center=(self.window_width // 2, self.window_height // 2 + 50))
             self.window.blit(volume_text, volume_text_rect)
 
